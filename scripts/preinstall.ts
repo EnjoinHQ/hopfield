54a56315f9045a706970c5f0ff7d407ca327579e
Chase Adams <c@cadams.io>
2023-10-03 15:48:08 -0700

    fix: whoops

--- scripts/preinstall.ts
+++ scripts/preinstall.ts
@@ -1,7 +1,7 @@
 import whichPmRuns from 'which-pm-runs';
 
 const pm = whichPmRuns();
-if (String(pm?.name)?.includes('bun'))
+if (!String(pm?.name)?.includes('bun'))
   throw new Error(
     `\`${pm?.name} install\` is not supported. Please run \`bun install\``,
   );
