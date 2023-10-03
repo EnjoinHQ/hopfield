5fd5e9fdb3cc7405b14a42c95c23a168787044db
Chase Adams <c@cadams.io>
2023-10-03 16:01:59 -0700

    fix: remove bun req

--- scripts/preinstall.ts
+++
@@ -1,7 +0,0 @@
-import whichPmRuns from 'which-pm-runs';
-
-const pm = whichPmRuns();
-if (!String(pm?.name)?.includes('bun'))
-  throw new Error(
-    `\`${pm?.name} install\` is not supported. Please run \`bun install\``,
-  );
