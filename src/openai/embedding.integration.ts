1208da4334408bb95e01d2668f0a2ae119310f08
Chase Adams <c@cadams.io>
2023-11-08 10:03:55 -0700

    test: timeout for embeddings

--- src/openai/embedding.integration.ts
+++ src/openai/embedding.integration.ts
@@ -8,22 +8,30 @@ const hopfield = hop.client(openai).provider(openAIClient);
 const embeddings = hopfield.embedding();
 const multipleEmbeddings = hopfield.embedding('text-embedding-ada-002', 3);
 
-test('should respond with embedding', async () => {
-  const response = await embeddings.get({
-    input: ['hopfield'],
-  });
+test(
+  'should respond with embedding',
+  async () => {
+    const response = await embeddings.get({
+      input: ['hopfield'],
+    });
 
-  expect(response.model).toMatchInlineSnapshot('"text-embedding-ada-002-v2"');
-  expect(response.data[0].embedding[0]).toBeCloseTo(-0.0073666335, 2);
-  expect(response.data[0].embedding[1535]).toBeCloseTo(-0.0013278616, 2);
-});
+    expect(response.model).toMatchInlineSnapshot('"text-embedding-ada-002-v2"');
+    expect(response.data[0].embedding[0]).toBeCloseTo(-0.0073666335, 2);
+    expect(response.data[0].embedding[1535]).toBeCloseTo(-0.0013278616, 2);
+  },
+  { timeout: 10_000 },
+);
 
-test('should respond with multiple embeddings', async () => {
-  const response = await multipleEmbeddings.get({
-    input: ['ready', 'set', 'hopfield'],
-  });
+test(
+  'should respond with multiple embeddings',
+  async () => {
+    const response = await multipleEmbeddings.get({
+      input: ['ready', 'set', 'hopfield'],
+    });
 
-  expect(response.model).toMatchInlineSnapshot('"text-embedding-ada-002-v2"');
-  expect(response.data[0].embedding[0]).toBeCloseTo(-0.009482461, 2);
-  expect(response.data[2].embedding[0]).toBeCloseTo(-0.0073666335, 2);
-});
+    expect(response.model).toMatchInlineSnapshot('"text-embedding-ada-002-v2"');
+    expect(response.data[0].embedding[0]).toBeCloseTo(-0.009482461, 2);
+    expect(response.data[2].embedding[0]).toBeCloseTo(-0.0073666335, 2);
+  },
+  { timeout: 10_000 },
+);
